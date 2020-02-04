42052ad07c63a462e3750418b3ab79db6a78f389
Thyago Mota <tmota@msudenver.edu>
2020-02-04 14:32:40 -0700

    updates

---
+++ homework_03_FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,37 @@
+/*
+ * CS 2050 - Computer Science II - Spring 2020
+ * Instructor: Thyago Mota
+ * Description: Homework 03 - FizzBuzz
+ */
+
+public class FizzBuzz {
+
+    private int current;
+    private static final int DEFAULT_START = 1;
+
+    // TODO: implement parameter-less constructor which should set the current number to the default start
+    public FizzBuzz() {
+
+    }
+
+    // TODO: implement parameterized constructor which should set the current number based on the user provided value; if start is less than 1, set current to the default start
+    public FizzBuzz(int start) {
+
+    }
+
+    // TODO: implement the getter method that should return the value of current
+    public int getCurrent() {
+        return DEFAULT_START;
+    }
+
+    // TODO: implement next which should increment current by 1 unit
+    public void next() {
+
+    }
+
+    // TODO: override toString which should return the current number as a string, or the words "Fizz", "Buzz", or "FizzBuzz" depending whether the number is a multiple of 3, 5, or both, respectively
+    @Override
+    public String toString() {
+        return "";
+    }
+}
