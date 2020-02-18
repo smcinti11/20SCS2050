1bedf4404b640a934ce0049321e198799a3f7012
Thyago Mota <tmota@msudenver.edu>
2020-02-18 07:02:38 -0700

    updates

--- activity_06_linked_list/src/LinkedList.java
+++ activity_06_linked_list/src/LinkedList.java
@@ -6,10 +6,24 @@ public class LinkedList {
         head = null;
     }
 
+    // add in front
     public void add(int data) {
         Node newNode = new Node(data);
         newNode.setNext(head);
         head = newNode;
+    }
+
+    // add tail
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (isEmpty())
+            head = newNode;
+        else {
+            Node current = head;
+            while (current.getNext() != null)
+                current = current.getNext();
+            current.setNext(newNode);
+        }
     }
 
     @Override
@@ -47,6 +61,57 @@ public class LinkedList {
             current = current.getNext();
         }
         return current.getData();
+    }
+
+    public void set(int index, int data) {
+        if (index < 0 || index >= size())
+            return;
+        int i = 0;
+        Node current = head;
+        while (i < index) {
+            i++;
+            current = current.getNext();
+        }
+        current.setData(data);
+    }
+
+    void insert(int index, int data) {
+        if (index < 0 || index >= size())
+            return;
+        if (index == 0)
+            add(data);
+        else {
+            Node newNode = new Node(data);
+            int i = 0;
+            Node current = head;
+            while (i < index - 1) {
+                i++;
+                current = current.getNext();
+            }
+            newNode.setNext(current.getNext());
+            current.setNext(newNode);
+        }
+    }
+
+    void remove(int index) {
+        if (index < 0 || index >= size())
+            return;
+        if (index == 0) {
+            Node temp = head;
+            head = head.getNext();
+            temp.setNext(null);
+        }
+        else {
+            int i = 0;
+            Node current = head;
+            while (i < index - 1) {
+                i++;
+                current = current.getNext();
+            }
+            Node temp = current.getNext();
+            current.setNext(current.getNext().getNext());
+            temp.setNext(null);
+        }
     }
 
 
--- activity_06_linked_list/src/LinkedListDriver.java
+++ activity_06_linked_list/src/LinkedListDriver.java
@@ -4,27 +4,13 @@ public class LinkedListDriver {
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
+        ll.append(3);
+//        ll.append(5);
+//        ll.append(8);
+//        ll.append(2);
         System.out.println(ll);
-        if (ll.isEmpty())
-            System.out.println("Is empty!");
-        else
-            System.out.println("Not empty!");
-        System.out.println("Size: " + ll.size());
-        ll.add(8);
+        ll.remove(0);
         System.out.println(ll);
-        ll.add(2);
-        System.out.println(ll);
-        ll.add(5);
-        System.out.println(ll);
-        if (ll.isEmpty())
-            System.out.println("Is empty!");
-        else
-            System.out.println("Not empty!");
-        System.out.println("Size: " + ll.size());
-
-        System.out.println(ll.get(0));
-        System.out.println(ll.get(1));
-        System.out.println(ll.get(2));
-        System.out.println(ll.get(3));
+        System.out.println(ll.isEmpty());
     }
 }
