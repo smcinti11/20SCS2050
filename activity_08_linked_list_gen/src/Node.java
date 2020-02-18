d8fecab9c6983da457020eb0e99dc973aec781cc
Thyago Mota <tmota@msudenver.edu>
2020-02-18 14:03:11 -0700

    updates

---
+++ activity_08_linked_list_gen/src/Node.java
@@ -0,0 +1,36 @@
+public class Node<T> {
+
+    private T  data;
+    private Node next;
+
+    public Node() {
+        data = null;
+        next = null;
+    }
+
+    public Node(T data) {
+        this.data = data;
+        next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return data + "";
+    }
+}
