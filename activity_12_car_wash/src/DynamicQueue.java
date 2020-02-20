c89e41c3ca01fa2e41eba54af70e5d432f485d65
Thyago Mota <tmota@msudenver.edu>
2020-02-20 16:38:37 -0700

    updates

---
+++ activity_12_car_wash/src/DynamicQueue.java
@@ -0,0 +1,31 @@
+public class DynamicQueue<T> {
+
+    public DynamicQueue() {
+
+    }
+
+    public void push(T data) {
+
+    }
+
+    public T pop() {
+        return null;
+    }
+
+    public T peek() {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
