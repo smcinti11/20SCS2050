a2607fb002981a8b386484772e8697d940255021
Thyago Mota <tmota@msudenver.edu>
2020-03-01 15:15:59 -0700

    update

---
+++ prg_01_google_maps/src/Route.java
@@ -0,0 +1,38 @@
+/*
+ * CS 2050 - Computer Science II - Spring 2020
+ * Instructor: Thyago Mota
+ * Description: Prg 01 - Route class
+ * Your name(s):
+ */
+
+public class Route {
+
+    // TODO: implement instance/class variables
+
+
+    // TODO: implement the parameter-less constructor
+    public Route() {
+
+    }
+
+    // TODO: return the number of waypoints in the route
+    public int size() {
+        return 0;
+    }
+
+    // TODO: a route should not accept jumps from one waypoint to another greater than MAX_KUMP; return true/false depending whether the waypoint was successfully added or not
+    public boolean add(final Waypoint waypoint) {
+        return false;
+    }
+
+    // TODO: return the waypoint at the specified index location
+    public Waypoint get(int index) {
+        return null;
+    }
+
+    // TODO: return a string representation of a route from its start to its end
+    @Override
+    public String toString() {
+        return "";
+    }
+}
